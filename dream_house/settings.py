a1d9a16f133c06103d621c5156bf999e6758063d
Pavel Vorobev <pavel.v.vorobev@gmail.com>
2024-01-29 23:21:57 -0600

    completed backend

--- dream_house/settings.py
+++ dream_house/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 cloudinary.config(
     cloud_name="dv6xqszj1",
     api_key="469967539622918",
@@ -100,12 +101,11 @@ WSGI_APPLICATION = 'dream_house.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+import dj_database_url
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse("postgres://dream_house_user:4zrBVjndyb0HBqeuViQw6vMKHLHpBbIM@dpg-cms8b28l5elc738n62u0-a.oregon-postgres.render.com/dream_house")
 }
 
 
@@ -143,7 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
